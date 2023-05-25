e88de718cb75e045897a070ca551f8dfc3688bd8
Vardhineni Kishor Babu <kishor.vardhineni@covalenseglobal.com>
2023-05-25 18:07:51 +0530

    display the questions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import Question from './Question.json';
 import Display from './Display';
+import { useState } from 'react';
 
 function App() {
-
+  let tempMarks = [];
+  const [marks, setMarks] = useState([]);
+  const changeHandler = (value, index) => {
+     tempMarks = marks;
+     tempMarks[index] = value;
+     setMarks([...tempMarks]);
+  }
   return (
     <div>
        <center>
          {Question.map((question, index) => {
             return (
-              <Display question={question} index={index}/>
+              <Display question={question} index={index} changeHandler={changeHandler}/>
             )
          })}
+         <br/>&nbsp;
+         <button className="quizBtn" onClick={() => alert(marks.reduce((a,b) => a+b))}>End Quiz</button>
+         <br/>
+         <br/>
        </center>
     </div>
   );
--- src/Display.js
+++ src/Display.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css'
 
-function Display({question, index}) {
+function Display({question, index, changeHandler}) {
   const handler = (e) => {
-    alert(e.target.value);
+    const temp = question.answer === e.target.value ? 1: 0;
+    changeHandler(temp, index);
   }
   return (
     <div className="alignLeft">
+      <div className='d-flex'>{index}
       <h3>{question.title}</h3>
-      <div onChange={handler}>
-        <input type="radio" value="A" name={index} /> {question.options[0]}&nbsp;
-        <input type="radio" value="B" name={index} /> {question.options[1]}&nbsp;
-        <input type="radio" value="C" name={index} /> {question.options[2]}&nbsp;
-        <input type="radio" value="D" name={index} /> {question.options[3]}&nbsp;
+      <div onChange={handler} style={{paddingLeft: "25px"}}>
+        ({1})<input type="radio" value="A" name={index} /> {question.options[0]}&nbsp;
+        ({2})<input type="radio" value="B" name={index} /> {question.options[1]}&nbsp;
+        ({3})<input type="radio" value="C" name={index} /> {question.options[2]}&nbsp;
+        ({4})<input type="radio" value="D" name={index} /> {question.options[3]}&nbsp;
+      </div>
       </div>
     </div>
   );
